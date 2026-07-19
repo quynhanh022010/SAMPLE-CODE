74d5f68d99cd8d413240a9818ae008f14cffbf0a
quynhanh022010 <quynhanh022010@gmail.com>
2026-07-19 20:43:12 +0700

    Refactor KMP algorithm and main function
    Refactor KMP algorithm implementation to improve clarity and efficiency.

--- KMP.cpp
+++ KMP.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 #define fors(i, a, b, c) for (int i=(a); i<=(b); i+=(c))
-#define ford(i, a, b, c) for (int i=(a); i>=(b); i-=(c))
+#define ford(i, a, b, c) for (int i=(a); i?=(b); i-=(c))
 #define pb push_back
 #define fi first
 #define se second
@@ -11,49 +11,30 @@ typedef unsigned long long ull;
 typedef double db;
 typedef long double ldb;
 
-int cnt = 0;
-
-vector<int> get_lps(str p) {
-    int m = p.size();
-    vector<int> lps(m, 0);
-    int len = 0;
-
-    fors(i, 1, m-1, 1) {
-        while(len > 0 && p[i] != p[len]) {
-            len = lps[len-1];
-        }
-        if (p[i] == p[len]) len++;
-        lps[i] = len;
-    }
-
-    return lps;
-}
-
-void kmp(str s, str p) {
-    int n = s.size();
-    int m = p.size();
-
-    vector<int> lps = get_lps(p);
-    int j = 0;
-
-    fors(i, 0, n-1, 1) {
-        while(j > 0 && s[i] != p[j]) {
-            j = lps[j-1];
-        }
-
-        if (s[i] == p[j]) j++;
-        if (j == m) {
-            cnt++;
-            j = lps[j - 1];
-        }
-    }
-    cout << cnt;
-}
-
 int main() {
     ios::sync_with_stdio(false); cin.tie(nullptr);
 
-    str s, t;
-    cin >> s >> t;
-    kmp(s, t);
+    str s, p;
+    cin >> s >> p;
+    int n = s.size(), m = p.size();
+
+    vector<int> lps(m, 0);
+    fors(i, 1, m-1, 1) {
+        int j = lps[i-1];
+        while(j > 0 && p[i] != p[j]) j = lps[j-1];
+        if (p[i] == p[j]) j++;
+        lps[i] = j;
+    }
+
+    int cnt = 0, j = 0;
+    fors(i, 0, n-1, 1) {
+        while(j > 0 && s[i] != p[j]) j = lps[j-1];
+        if (s[i] == p[j]) j++;
+        if (j == m) {
+            cnt++;
+            j = lps[j-1];
+        }
+    }
+
+    cout << cnt;
 }
